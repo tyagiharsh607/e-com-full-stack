c4ba5dfcb4635729032b70d1e20172613df97132
tyagiharsh607 <tyagiharsh607@gmail.com>
2023-09-10 11:08:06 +0530

    test

--- frontend/src/Slices/apiSlice.js
+++ frontend/src/Slices/apiSlice.js
@@ -8,16 +8,3 @@ export const apiSlice = createApi({
   tagTypes: ["Products", "Users", "Orders"],
   endpoints: (builder) => ({}),
 });
-
-function addTwoNumbers(a, b) {
-  return a + b;
-}
-
-function multiplyTwoNumbers(a, b) {
-  return a * b;
-}
-function averageTwoNumbers(a, b) {
-  return (a + b) / 2;
-}
-
-export const { useGetProductsQuery } = apiSlice;
