15889d71db082ba58e815bda64fd469e9c80f000
harsh <tyagiharsh607@gmail.com>
2024-09-26 17:13:35 +0530

    added a server call every 30 seconds to keep it alivein the backend

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,36 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
+
+import axios from "axios";
+
 const app = express();
 dotenv.config();
+
+const url = `https://e-com-hk31.onrender.com/`;
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
 
 app.use(
   cors({
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -36,30 +36,7 @@ import UserEditPage from "./Pages/Admin/UserEditPage";
 import AdminRoute from "./components/AdminRoute";
 import PrivateRoute from "./components/PrivateRoute";
 
-import axios from "axios";
 
-const url = `https://e-com-hk31.onrender.com/`;
-const interval = 30000; // Interval in milliseconds (30 seconds)
-
-function reloadWebsite() {
-  axios
-    .get(url)
-    .then((response) => {
-      console.log(
-        `Reloaded at ${new Date().toISOString()}: Status Code ${
-          response.status
-        }`
-      );
-    })
-    .catch((error) => {
-      console.error(
-        `Error reloading at ${new Date().toISOString()}:`,
-        error.message
-      );
-    });
-}
-
-setInterval(reloadWebsite, interval);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
