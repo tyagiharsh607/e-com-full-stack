2122c77e58c069f49ea2c5b91bde794cb139d87b
harsh <tyagiharsh607@gmail.com>
2024-09-26 16:30:34 +0530

    added a server call every 30 seconds to keep it alive

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -36,6 +36,31 @@ import UserEditPage from "./Pages/Admin/UserEditPage";
 import AdminRoute from "./components/AdminRoute";
 import PrivateRoute from "./components/PrivateRoute";
 
+import axios from "axios";
+
+const url = `https://e-com-hk31.onrender.com/`;
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
